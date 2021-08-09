df8b8d1badb8e0771c46fcb42e47b6385a83f143
Ariel Don <arieldn@protonmail.com>
2021-08-09 12:37:48 -0500

    Implement cell death

--- life.c
+++ life.c
@@ -5,15 +5,33 @@
 #include <string.h>
 #include <time.h>
 
-#define CELL_ALIVE ACS_BLOCK
-#define CELL_DEAD  ACS_BULLET
-#define CELL_FUNC  (rand() % 50 ? CELL_DEAD : CELL_ALIVE)
+#define CELL_ALIVE      ACS_BLOCK
+#define CELL_DEAD       ACS_BULLET
+#define CELL_EXPR       (rand() % 50 ? CELL_DEAD : CELL_ALIVE)
+#define CELL_EXPR_INV   (rand() % 50 ? CELL_ALIVE : CELL_DEAD)
+
+static size_t
+count_neighbors(size_t line, size_t col)
+{
+    size_t neighbors = 0;
+
+    neighbors += mvinch(line - 1, col) == CELL_ALIVE;
+    neighbors += mvinch(line - 1, col + 1) == CELL_ALIVE;
+    neighbors += mvinch(line, col + 1) == CELL_ALIVE;
+    neighbors += mvinch(line + 1, col + 1) == CELL_ALIVE;
+    neighbors += mvinch(line + 1, col) == CELL_ALIVE;
+    neighbors += mvinch(line + 1, col - 1) == CELL_ALIVE;
+    neighbors += mvinch(line, col - 1) == CELL_ALIVE;
+    neighbors += mvinch(line - 1, col - 1) == CELL_ALIVE;
+
+    return neighbors;
+}
 
 static void
 init_life(void)
 {
     for (;;) {
-        chtype ch = CELL_FUNC;
+        chtype ch = CELL_EXPR;
         if (addch(ch) == ERR) {
             break;
         }
@@ -26,18 +44,26 @@ iter_life(void)
     for (ssize_t line = 0; line < LINES; ++line) {
         for (ssize_t col = 0; col < COLS; ++col) {
             if (mvinch(line, col) == CELL_ALIVE) {
+                /* A cell on the edge can die. */
+                if (count_neighbors(line, col) < 8) {
+                    addch(CELL_EXPR_INV);
+                    if (inch() == CELL_DEAD) {
+                        continue;
+                    }
+                }
+
                 /*
                  * mvinch() simply returns ERR if passed a position
                  * out-of-bounds -- it doesn't crash.
                  */
                 if (mvinch(line + 1, col) == CELL_DEAD) {
-                    addch(CELL_FUNC);
+                    addch(CELL_EXPR);
                 } else if (mvinch(line, col + 1) == CELL_DEAD) {
-                    addch(CELL_FUNC);
+                    addch(CELL_EXPR);
                 } else if (mvinch(line - 1, col) == CELL_DEAD) {
-                    addch(CELL_FUNC);
+                    addch(CELL_EXPR);
                 } else if (mvinch(line, col - 1) == CELL_DEAD) {
-                    addch(CELL_FUNC);
+                    addch(CELL_EXPR);
                 }
             }
         }
